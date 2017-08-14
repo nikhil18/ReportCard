36fcbdc0419f1581b69930989c29f2d89defcab9
nikhil18 <nikhilgolechha1@gmail.com>
2017-08-14 22:45:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/reportcard/reportCard.java
@@ -0,0 +1,116 @@
+package com.example.android.reportcard;
+
+/**
+ * Report Card class
+ *
+ */
+public class reportCard {
+    private char mSection;
+    private String mName;
+    private int mClass;
+    private int mGeography;
+    private int mHistory;
+    private int mPhysics;
+    private int mChemistry;
+    private int mMaths;
+    private int sum;
+
+    public reportCard(String name, int class_, char section, int history, int geography,
+                      int physics, int chemistry, int maths) {
+        mName = name;
+        mClass = class_;
+        mSection = section;
+        mGeography = geography;
+        mMaths = maths;
+        mHistory = history;
+        mPhysics = physics;
+        mChemistry = chemistry;
+    }
+
+    // Get name of Student
+    public String getName() {
+        return mName;
+    }
+    // Set Name of Student
+    public void setName(String name) {
+        mName = name;
+    }
+    // Get class of student
+    public int getClass_() {
+        return mClass;
+    }
+    // Set class of student
+    public void setClass(int class_) {
+        mClass = class_;
+    }
+    // Get section of student
+    public char getSection() {
+        return mSection;
+    }
+    // Set section of student
+    public void setSection(char section) {
+        mSection = section;
+    }
+    // Get physics marks
+    public int getPhysicsMarks() {
+        return mPhysics;
+    }
+    // Set physics marks
+    public void setPhysicsMarks(int physics) {
+        mPhysics = physics;
+    }
+    // Get history marks
+    public int getHistoryMarks() {
+        return mHistory;
+    }
+    // Set history marks
+    public void setHistoryMarks(int history) {
+        mHistory = history;
+    }
+    // Get geography marks
+    public int getGeographyMarks() {
+        return mGeography;
+    }
+    // Set geography marks
+    public void setGeographyMarks(int geography) {
+        mGeography = geography;
+    }
+    // Get maths marks
+    public int getMathsMarks() {
+        return mMaths;
+    }
+    // Set maths marks
+    public void setMathsMarks(int maths) {
+        mMaths = maths;
+    }
+    // Get chemistry marks
+    public int getChemistryMarks() {
+        return mChemistry;
+    }
+    // Set chemistry marks
+    public void setChemistryMarks(int chemistry) {
+        mChemistry = chemistry;
+    }
+
+    // get total marks
+    public int getTotalMarks() {
+        sum = mChemistry + mGeography + mMaths + mPhysics +
+                mHistory;
+        return sum;
+    }
+
+    public double getPercentage() {
+        return (sum/5);
+    }
+
+    @Override
+    public String toString() {
+        return "Result { " +
+                "History = " + mHistory +
+                ", Geography = " + mGeography +
+                ", Computer Science = " + mMaths +
+                ", Chemistry = " + mChemistry +
+                ", Physics = " + mPhysics +
+                " }";
+    }
+}
